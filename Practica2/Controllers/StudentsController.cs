58ab87c7d49f22d08e581f163d9ff0359f54cb45
Sofía Vargas <sofi@Sofias-MacBook-Air-1139.local>
2021-04-11 16:43:31 -0400

    P2-002 Added files Student and StudentController

---
+++ Practica2/Controllers/StudentsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Practica2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentsController : ControllerBase
+    {
+        public StudentsController()
+        {
+        }
+    }
+}
